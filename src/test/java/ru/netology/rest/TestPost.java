08a3b54bfdd77e242de840d8da1d05d51442e3f4
Alex Deyanov <deyanovan@yandex.ru>
2024-03-10 15:26:01 +0300

    Task 3 is done, tests is ok

---
+++ src/test/java/ru/netology/rest/TestPost.java
@@ -0,0 +1,22 @@
+package ru.netology.rest;
+
+import org.junit.jupiter.api.Test;
+
+import static io.restassured.RestAssured.given;
+import static org.hamcrest.Matchers.equalTo;
+
+public class TestPost {
+    @Test
+    public void test1() {
+        given()
+                .baseUri("https://postman-echo.com")
+                .contentType("text/plain; charset=UTF-8")
+                .body("Привет")
+                .when()
+                .post("/post")
+                .then()
+                .statusCode(200)
+                .body("data", equalTo("Привет"))
+        ;
+    }
+}
